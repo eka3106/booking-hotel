a84765bd909077630191faf73a8eb66571dac66e
exa31 <bloodsuker18@gmail.com>
2024-12-27 06:09:22 +0700

    ganti url /auth jadi /api

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "booking-hotel/config"
 	"booking-hotel/databases"
+	_ "booking-hotel/docs"
 	"booking-hotel/middleware"
 	"booking-hotel/modules/booking"
 	"booking-hotel/modules/fasilitas"
@@ -17,16 +18,23 @@ import (
 	"booking-hotel/modules/user"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/swagger"
 )
+
+// @title Reservasi Hotel API
+// @version 1.0
+// @description API untuk reservasi hotel
+// @BasePath /api/1.0
 
 func main() {
 
 	defer databases.SqlDb.Close()
 
 	fiberApp := fiber.New()
-
+	fiberApp.Use(cors.New())
+	fiberApp.Get("/docs/*", swagger.HandlerDefault)
 	fiberApp.Use(middleware.AuthJWT())
-
 	booking.RouterBooking(fiberApp)
 	fasilitas.RouterFasilitas(fiberApp)
 	fasilitashotel.RouterFasilitasHotel(fiberApp)
--- modules/user/user.controller.go
+++ modules/user/user.controller.go
@@ -3,7 +3,7 @@ package user
 import "github.com/gofiber/fiber/v2"
 
 func RouterUser(app *fiber.App) {
-	router := app.Group("/auth/1.0")
+	router := app.Group("/api/1.0")
 	router.Post("/register", CreateUser)
 	router.Post("/register-admin-hotel", CreateAdminHotel)
 	router.Post("/login", Login)
