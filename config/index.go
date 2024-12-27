6c8f2b50fbcf6174c151b265adea965b5960b6f8
exa31 <bloodsuker18@gmail.com>
2024-12-27 19:24:31 +0700

    redeploy

--- config/index.go
+++ config/index.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/viper"
 )
@@ -15,4 +16,12 @@ func init() {
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println("Error reading config file", err)
 	}
+
+	viper.SetDefault("PORT", os.Getenv("PORT"))
+	viper.SetDefault("DB_HOST", os.Getenv("DB_HOST"))
+	viper.SetDefault("DB_PORT", os.Getenv("DB_PORT"))
+	viper.SetDefault("DB_USER", os.Getenv("DB_USER"))
+	viper.SetDefault("DB_PASSWORD", os.Getenv("DB_PASSWORD"))
+	viper.SetDefault("DB_NAME", os.Getenv("DB_NAME"))
+	viper.SetDefault("JWT_SECRET", os.Getenv("JWT_SECRET"))
 }
