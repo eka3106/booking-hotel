0da304c86b790406f0788d58ff14eb4b0e943adf
ekaaaa31064 <ekaaa.jobs@gmail.com>
2024-12-27 10:32:16 +0700

    ganti databases connection with aiven

--- databases/index.go
+++ databases/index.go
@@ -3,6 +3,8 @@ package databases
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/url"
 
 	"github.com/spf13/viper"
 	"gorm.io/driver/postgres"
@@ -15,16 +17,39 @@ var (
 )
 
 func init() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", viper.GetString("DB_HOST"), viper.GetString("DB_USER"), viper.GetString("DB_PASSWORD"), viper.GetString("DB_NAME"), viper.GetInt("DB_PORT"))
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Println("Error connecting to database", err)
-	}
-	sqlDB, err := db.DB()
-	if err != nil {
-		fmt.Println("Error connecting to database", err)
-	}
-	fmt.Println("Database connected")
+	viper.SetConfigFile(".env")
+    viper.AutomaticEnv()
+    if err := viper.ReadInConfig(); err != nil {
+        log.Fatalf("Error reading config file, %s", err)
+    }
+
+    // Membuat string koneksi
+    serviceURI := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=require",
+        viper.GetString("DB_USER"),
+        viper.GetString("DB_PASSWORD"),
+        viper.GetString("DB_HOST"),
+        viper.GetInt("DB_PORT"),
+        viper.GetString("DB_NAME"),
+    )
+
+    conn, err := url.Parse(serviceURI)
+    if err != nil {
+        log.Fatalf("Error parsing service URI, %s", err)
+    }
+    conn.RawQuery = "sslmode=verify-ca&sslrootcert=ca.pem"
+
+    // Membuka koneksi dengan GORM
+    db, err := gorm.Open(postgres.Open(conn.String()), &gorm.Config{})
+    if err != nil {
+        log.Fatalf("Error connecting to database, %s", err)
+    }
+
+    // Mendapatkan koneksi SQL dari GORM
+    sqlDB, err := db.DB()
+    if err != nil {
+        log.Fatalf("Error getting SQL DB from GORM, %s", err)
+    }
+
 	DB = db
 	SqlDb = sqlDB
 }
